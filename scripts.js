8cf4ddda083c6ac094052f45534896565ef4f800
RadiantSolarSolutions <radiantsolarsolutions.pa@gmail.com>
2024-09-09 18:25:36 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,31 +1,18 @@
 document.getElementById('quote-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent default form submission
 
-    const name = document.getElementById('name').value;
-    const phone = document.getElementById('phone').value;
-    const email = document.getElementById('email').value;
-    const system = document.getElementById('system').value;
-    const otherSystem = document.getElementById('otherSystemInput').value;
-    const arrays = document.getElementById('arrays').value;
-    const stories = document.getElementById('stories').value;
-    const description = document.getElementById('description').value;
-
-    const templateParams = {
-        name: name,
-        phone: phone,
-        email: email,
-        system: system === 'OTHER' ? otherSystem : system,
-        arrays: arrays,
-        stories: stories,
-        description: description
-    };
-
-    emailjs.send("service_9oxov8m", "template_iah4myb", templateParams)
-        .then(function(response) {
-            //console.log('SUCCESS!', response.status, response.text);
+    fetch(this.action, {
+        method: this.method,
+        body: new FormData(this),
+        headers: { 'Accept': 'application/json' }
+    }).then(response => {
+        if (response.ok) {
             alert('SUCCESS! Your quote request has been sent.');
-        }, function(error) {
-            //console.error('FAILED...', error);
+            this.reset();
+        } else {
             alert('FAILED... Please try again later.');
-        });
+        }
+    }).catch(error => {
+        alert('FAILED... Please try again later.');
+    });
 });
